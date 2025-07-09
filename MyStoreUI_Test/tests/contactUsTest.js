5624ab429abb6f2dbbc7e90018b3530c10283472
YaraKhalid <63379805+YaraKhalid@users.noreply.github.com>
2025-07-09 19:12:15 +0300

    Update contactUsTest.js

--- MyStoreUI_Test/tests/contactUsTest.js
+++ MyStoreUI_Test/tests/contactUsTest.js
@@ -16,7 +16,7 @@ module.exports = {
     console.log('Starting test suite...');
   },
    beforeEach: function (browser) {
-    browser.page.contactUsPage().navigate().waitForElementVisible('@emailInput', 5000);
+    browser.page.ContactUsPage().navigate().waitForElementVisible('@emailInput', 5000);
   },
 
   after: function (browser) {
@@ -24,35 +24,35 @@ module.exports = {
   },
 
   'TC1: Submit Contact Us Form with file upload Successfully': function (browser) {
-    const contactUs = browser.page.contactUsPage();
+    const contactUs = browser.page.ContactUsPage();
     contactUs
       .fillContactForm(subjectHeading, email, message, true)
       .assertSuccessMessage();
   },
 
   'TC2: Submit Contact Form with invalid email': function (browser) {
-    const contactUs = browser.page.contactUsPage();
+    const contactUs = browser.page.ContactUsPage();
     contactUs
       .fillContactForm(subjectHeading, invalidEmail, message)
       .assertErrorMessage('Invalid email address.');
   },
 
   'TC3: Submit Contact Form without subject heading': function (browser) {
-    const contactUs = browser.page.contactUsPage();
+    const contactUs = browser.page.ContactUsPage();
     contactUs
       .fillContactForm('', email, message)
       .assertErrorMessage('Please select a subject from the list provided.');
   },
 
   'TC4: Submit Contact Form without message': function (browser) {
-    const contactUs = browser.page.contactUsPage();
+    const contactUs = browser.page.ContactUsPage();
     contactUs
       .fillContactForm(subjectHeading, email, '')
       .assertErrorMessage('The message cannot be blank.');
   },
 
   'TC5: Submit Contact Form with all mandatory fields missing': function (browser) {
-    const contactUs = browser.page.contactUsPage();
+    const contactUs = browser.page.ContactUsPage();
     contactUs
       .fillContactForm('', '', '')
       .assertErrorMessage('Invalid email address.');
@@ -60,28 +60,28 @@ module.exports = {
 
 
   'TC6: Submit Contact Form successfully without file upload': function (browser) {
-    const contactUs = browser.page.contactUsPage();
+    const contactUs = browser.page.ContactUsPage();
     contactUs
       .fillContactForm(subjectHeading, email, message)
       .assertSuccessMessage();
   },
 
   'TC7: Submit Contact Form with minimum letters in message': function (browser) {
-    const contactUs = browser.page.contactUsPage();
+    const contactUs = browser.page.ContactUsPage();
     contactUs
       .fillContactForm(subjectHeading, email, shortMessage)
       .assertSuccessMessage();
   },
 
   'TC8: Submit Contact Form with 50,000 characters in message': function (browser) {
-    const contactUs = browser.page.contactUsPage();
+    const contactUs = browser.page.ContactUsPage();
     contactUs
       .fillContactForm(subjectHeading, email, longMessage)
       .assertSuccessMessage();
   },
 
   'TC9: Submit Contact Form with empty email field': function (browser) {
-    const contactUs = browser.page.contactUsPage();
+    const contactUs = browser.page.ContactUsPage();
     contactUs
       .fillContactForm(subjectHeading, '', message)
       .assertErrorMessage('Invalid email address.');
